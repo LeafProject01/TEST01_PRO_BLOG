4a81d66ba47af0d97fccd2ece5fd6f0c003c81be
Nakanishi <r.nakanishi@lincrea.co.jp>
2011-12-15 23:42:31 +0900

    new app

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,89 @@
+class CommentsController < ApplicationController
+  before_filter :load_post
+
+  def load_post
+    @post = Post.find(params[:post_id])
+  end
+
+  # GET /comments
+  # GET /comments.json
+  def index
+    @comments = Comment.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @comments }
+    end
+  end
+
+  # GET /comments/1
+  # GET /comments/1.json
+  def show
+    @comment = @post.comments.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @comment }
+    end
+  end
+
+  # GET /comments/new
+  # GET /comments/new.json
+  def new
+    @comment = @post.comments.build
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @comment }
+    end
+  end
+
+  # GET /comments/1/edit
+  def edit
+    @comment = @post.comments.find(params[:id])
+  end
+
+  # POST /comments
+  # POST /comments.json
+  def create
+    @comment = @post.comments.build(params[:comment])
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /comments/1
+  # PUT /comments/1.json
+  def update
+    @comment = @post.comments.find(params[:id])
+
+    respond_to do |format|
+      if @comment.update_attributes(params[:comment])
+        format.html { redirect_to [@post, @comment], notice: 'Comment was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to post_comments_url(@post) }
+      format.json { head :ok }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9c497616fdba1e8ba3b865edaf798f258401a1909b54b6b084976f0b742b5b77944aae09b213c651e0f9a7ae792120c1bb7f487477c0394c3643da541bf1ce5d'
